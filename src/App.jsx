d7acc8a1c664a56d8163cf14adf952e5c1f26fac
rafahuqe <arinahuqe23@gmail.com>
2023-09-13 17:14:50 +0600

    first react work

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header/Header'
+import Bottles from './Components/Bottles/Bottles'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+    <Bottles></Bottles>
+    </>
+  )
+}
+
+export default App
